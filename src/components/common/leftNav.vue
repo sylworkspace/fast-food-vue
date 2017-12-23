d21a5c4390c4e2661d44d859f827d9a44fb7ca51
syl <1085284022@qq.com>
2017-12-23 17:35:42 +0800

    add fast food pos style

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="left-nav">
+    <ul>
+      <li>
+        <i class="icon iconfont icon-goumai"></i>
+        <div>收银</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-dianpu"></i>
+        <div>店铺</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-icon-test"></i>
+        <div>商品</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-huiyuanqia"></i>
+        <div>会员</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-pinpaizhuanxiang"></i>
+        <div>统计</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+  .left-nav{
+      color: #fff;
+      font-size: 10px;
+      height: 100%;
+      background-color: #20a0ff;
+      float: left;
+      width: 5%;
+  }
+  .iconfont{
+      font-size: 24px;
+  }
+  .left-nav ul{
+      padding: 0px;
+      margin: 0px;
+  }
+  .left-nav ul li{
+      list-style: none;
+      text-align: center;
+      border-bottom: 1px solid #deed8c;
+      padding: 10px;
+  }
+</style>
